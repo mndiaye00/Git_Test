b2e502673f5cb4feae76951a4aa32ad08aeff4bc
mndiaye00 <msanes13@gmail.com>
2019-04-09 19:25:59 -0400

    Commit Test.java on my branch

---
+++ Git_Test/src/dev/moussa/main/Test.java
@@ -0,0 +1,14 @@
+package dev.moussa.main;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		sayHello();
+	}
+	
+	public static void sayHello() {
+		for(int i=0; i<10; i++) {
+			System.out.println("Value = " + i);
+		}
+	}
+}
